85ab704fe48a0ac22fb7b76efdd45368f21be404
Wiktor L <wiktor.langier7@gmail.com>
2024-10-07 13:53:01 +0200

    Add IEmployeeRepository interface and implement it

---
+++ AssetTracker/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using AssetTracker.Models;
+
+namespace AssetTracker.Interfaces;
+
+public interface IEmployeeRepository
+{
+    Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+    Task<Employee> GetEmployeeByIdAsync(int id);
+    Task CreateEmployeeAsync(Employee employee);
+    Task UpdateEmployeeAsync(Employee employee);
+    Task DeleteEmployeeAsync(int id);
+}
---
+++ AssetTracker/Repository/EmployeeRepository.cs
@@ -0,0 +1,61 @@
+﻿using AssetTracker.Data;
+using AssetTracker.Interfaces;
+using AssetTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetTracker.Repository;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext _context;
+    public EmployeeRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+    {
+        if (_context.Employees == null) {
+            throw new InvalidOperationException("Entity set 'Employees' is null");
+        }
+
+        return await _context.Employees.ToListAsync();
+    }
+    public async Task<Employee> GetEmployeeByIdAsync(int id)
+    {
+        if (_context.Employees == null)
+        {
+            throw new InvalidOperationException("Entity set 'Employees' is null");
+        }
+
+        var employee = await _context.Employees.Include(fa => fa.FixedAssets).SingleOrDefaultAsync(e => e.Id == id);
+
+        if (employee == null)
+        {
+            throw new InvalidOperationException("Employee does not exist");
+        }
+
+        return employee;
+
+    }
+    public async Task CreateEmployeeAsync(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();  
+    }
+    public async Task UpdateEmployeeAsync(Employee employee)
+    {
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+    }
+    public async Task DeleteEmployeeAsync(int id)
+    {
+        var existingEmployee = await _context.Employees.FirstOrDefaultAsync(e => e.Id == id);
+        
+        if(existingEmployee != null) 
+        {
+            _context.Remove(existingEmployee);
+            await _context.SaveChangesAsync();
+        }
+    }
+   
+}
