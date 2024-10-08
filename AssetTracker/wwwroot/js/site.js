f6bb1befc508f48575d51bf760f59edf9f35a624
Wiktor L <wiktor.langier7@gmail.com>
2024-10-08 15:38:52 +0200

    Add pagination, searching and sorting in Employee index view using DataTables

--- AssetTracker/wwwroot/js/site.js
+++ AssetTracker/wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
 // Write your JavaScript code.
+
+$(function () {
+    new DataTable('#EmployeeTable', {
+        "columnDefs": [
+            { "orderable": false, "target": 4 }
+        ],
+        "language": {
+            "search": "Wyszukaj",
+            "info": "Wyświetlanie _START_ do _END_ z _TOTAL_ wierszy",
+            "lengthMenu": "Wyświetl _MENU_ wierszy na stronę",
+        },
+    });
+
+});
