a4f3068442e071fb8a5b4e5b074766efec2b8a24
Wiktor L <wiktor.langier7@gmail.com>
2024-10-15 13:06:19 +0200

    Add and implement Fixed Asset Repository

---
+++ AssetTracker/Interfaces/IFixedAssetRepository.cs
@@ -0,0 +1,12 @@
+﻿using AssetTracker.Models;
+
+namespace AssetTracker.Interfaces;
+
+public interface IFixedAssetRepository
+{
+    Task<IEnumerable<FixedAsset>> GetAllAssetsAsync();
+    Task<FixedAsset> GetAssetByIdAsync(int id);
+    Task CreateFixedAsset(FixedAsset fixedAsset);
+    Task UpdateFixedAsset(FixedAsset fixedAsset);
+    Task DeleteFixedAssetAsync(int id);
+}
---
+++ AssetTracker/Repository/FixedAssetRepository.cs
@@ -0,0 +1,65 @@
+﻿using AssetTracker.Data;
+using AssetTracker.Interfaces;
+using AssetTracker.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetTracker.Repository;
+
+public class FixedAssetRepository : IFixedAssetRepository
+{
+    private readonly AppDbContext _context;
+    public FixedAssetRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<FixedAsset>> GetAllAssetsAsync()
+    {
+        if(_context.FixedAssets == null)
+        {
+            throw new InvalidOperationException("Entity set 'FixedAssets' is null");
+        }
+
+        return await _context.FixedAssets.ToListAsync();
+    }
+    public async Task<FixedAsset> GetAssetByIdAsync(int id)
+    {
+        if (_context.FixedAssets == null)
+        {
+            throw new InvalidOperationException("Entity set 'FixedAssets' is null");
+        }
+
+        var fixedAsset = await _context.FixedAssets.SingleOrDefaultAsync(fa => fa.Id == id);
+
+        if (fixedAsset == null)
+        {
+            throw new InvalidOperationException("Cannot find this asset");
+        }
+
+        return fixedAsset;
+    }
+    public async Task CreateFixedAsset(FixedAsset fixedAsset)
+    {
+        _context.FixedAssets.Add(fixedAsset);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateFixedAsset(FixedAsset fixedAsset)
+    {
+        _context.FixedAssets.Update(fixedAsset);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteFixedAssetAsync(int id)
+    {
+        var fixedAsset = await _context.FixedAssets.SingleOrDefaultAsync(fa => fa.Id == id);
+
+        if(fixedAsset != null)
+        {
+            _context.FixedAssets.Remove(fixedAsset);
+            await _context.SaveChangesAsync();
+        }
+
+        throw new InvalidOperationException("Cannot find this asset");
+    }
+}
