07e366571ffab3045201ed3230eb45b8e73b6f11
Wiktor L <wiktor.langier7@gmail.com>
2024-11-22 15:31:14 +0100

    Add chart.js library and create bar chart with it in dashboard.js

---
+++ AssetTracker/wwwroot/js/dashboard.js
@@ -0,0 +1,35 @@
+﻿
+$(function () {
+
+    // Bar chart for dashboard
+    $.ajax({
+        url: '/Dashboard/EmployeesWithTheMostAssets',
+        type: 'GET',
+        success: function (response) {
+
+            let labels = response.map(item => item.employeeName);
+            let dataPoints = response.map(item => item.assetCount);
+
+            let ctx = document.getElementById('barChart');
+
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Pracownicy z największą ilością zasobów',
+                        data: dataPoints,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            })
+        }
+    })
+});
