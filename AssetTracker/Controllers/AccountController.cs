f286bf8c32f074692a60848130d1d13b6436cfb3
Wiktor L <wiktor.langier7@gmail.com>
2024-12-18 21:28:31 +0100

    Add form for registration in register view and register get method in account controller

--- AssetTracker/Controllers/AccountController.cs
+++ AssetTracker/Controllers/AccountController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AssetTracker.Interfaces;
+using AssetTracker.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AssetTracker.Controllers;
 
 public class AccountController : Controller
 {
-    public IActionResult Register()
+    private readonly IEmployeeRepository _employeeRepository;
+    public AccountController(IEmployeeRepository employeeRepository)
     {
-        return View();
+        _employeeRepository = employeeRepository;
+    }
+    public async Task<IActionResult> Register()
+    {
+        var employees = await _employeeRepository.GetAllEmployeesAsync();
+
+        var selectList = employees.Select(e => new SelectListItem
+        {
+            Value = e.Email,
+            Text = e.Email
+        })
+        .ToList();
+
+        var model = new RegisterViewModel()
+        {
+            EmployeeEmails = selectList
+        };
+
+        return View(model);
     }
 }
