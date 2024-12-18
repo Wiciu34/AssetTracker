67e33fd8928f661b3834d9193cae828310f70c32
Wiktor L <wiktor.langier7@gmail.com>
2024-12-18 20:57:07 +0100

    Add register view model

---
+++ AssetTracker/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssetTracker.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Hasła nie są takie same")]
+    public string? ConfirmPassword { get; set; }
+    public IEnumerable<SelectListItem>?  EmployeeEmails { get; set; }
+}
