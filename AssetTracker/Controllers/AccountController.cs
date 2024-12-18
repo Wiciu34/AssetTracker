7f4736c43d5c1aa1314f09b6d3433d16e0803829
Wiktor L <wiktor.langier7@gmail.com>
2024-12-18 20:49:45 +0100

    Add acount controller and register view

---
+++ AssetTracker/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssetTracker.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
